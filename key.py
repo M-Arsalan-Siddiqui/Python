623d4cbd9d36ad2f154ab4da94784a06a58f05d6
M-Arsalan-Siddiqui <55347162+M-Arsalan-Siddiqui@users.noreply.github.com>
2020-12-07 23:25:38 +0500

    Add files via upload
    Auto press Key of Keyboard

---
+++ key.py
@@ -0,0 +1,22 @@
+from pynput.keyboard import Key ,Controller
+
+i=1
+
+while i>0:
+    keyboard=Controller()
+
+    Key=Key.enter
+    keyboard.press(Key)
+    keyboard.release(Key)
+
+
+
+
+
+
+
+
+
+
+
+   
